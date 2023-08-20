8b82247f18302772c26178e0f13adfb1274cc885
Sravan <tsravangoud164@gmail.com>
2023-08-20 17:11:26 +0530

    2 files modified

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -21,17 +21,45 @@ export default defineConfig({
 
     sidebar: [
       {
-        // text: 'Guide',
+        text: 'Guide',
         items: [
           { text: 'Example', link: '/example' },
-          // ...
+          { text: 'Developers', link: '/developers' }
         ],
       },
+      {
+        text: 'Section Title A',
+        items: [
+          { text: 'Item A', link: '/item-a' },
+          { text: 'Item B', link: '/item-b' },
+        ]
+      },
+      {
+        text: 'Section Title B',
+        items: [
+          { text: 'Item C', link: '/item-c' },
+          { text: 'Item D', link: '/item-d' },
+        ]
+      }
     ],
 
     footer: {
       message: 'Released under the <a href="https://github.com/tsra0ne/scodehub/blob/main/LICENSE">MIT License</a>.',
       copyright: 'Copyright © 2023 <a href="https://github.com/tsra0ne/scodehub">Swift Code Hub</a>'
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/tsra0ne/scodehub' },
+      { icon: 'twitter', link: 'https://twitter.com/scodehub404' },
+    ],
+
+    editLink: {
+      pattern: 'https://github.com/tsra0ne/scodehub/blob/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    },
+
+    search: {
+      provider: 'local'
     },
   },
 });
